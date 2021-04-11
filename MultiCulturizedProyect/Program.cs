cdc88a972d72a2dbc3301411e928e52c110198fd
Hernan Alvarez <halvarez@Austranet.com>
2021-04-11 12:18:26 -0400

    Se cambio de nombre la solucion, y se agrego un proyecto para verificar como tener recursos multiidioma

---
+++ MultiCulturizedProyect/Program.cs
@@ -0,0 +1,29 @@
+﻿using MultiCulturizedProyect.Properties;
+using System;
+using System.Globalization;
+using System.Threading;
+using static System.Console;
+
+namespace MultiCulturizedProyect
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Cultura default");
+            WriteWelcomes();
+            WriteLine("----------------------------------------------");
+
+            Resources.Culture = new CultureInfo("es-US");
+            WriteLine("Cultura en ingles");
+            WriteWelcomes();
+            WriteLine("----------------------------------------------");
+        }
+
+        static void WriteWelcomes()
+        {
+            WriteLine(string.Format(Resources.Hello, "Hernán"));
+            WriteLine(string.Format(Resources.Bye, "Hernán"));
+        }
+    }
+}
